f28efc58250161f3238f6be5946abef968c19cc9
Jakub Vsetecka <jvsetecka@segment1.org>
2023-12-04 08:45:56 +0100

    feat: working calculator

--- python.py
+++ python.py
@@ -13,3 +13,31 @@ def divide(num1, num2):
     else:
         return "Error: Division by zero is not allowed"
 
+while True:
+    print("Select operation -\n" \
+            "1. Add\n" \
+            "2. Subtract\n" \
+            "3. Multiply\n" \
+            "4. Divide\n" \
+            "5. Quit")
+
+    select = int(input("Select operations form 1, 2, 3, 4, 5 :"))
+
+    if select == 5:
+        break
+
+    number_1 = int(input("Enter first number: "))
+    number_2 = int(input("Enter second number: "))
+
+    if select == 1:
+        print(number_1, "+", number_2, "=", add(number_1, number_2))
+
+    elif select == 2:
+        print(number_1, "-", number_2, "=", subtract(number_1, number_2))
+
+    elif select == 3:
+        print(number_1, "*", number_2, "=", multiply(number_1, number_2))
+
+    elif select == 4:
+        print(number_1, "/", number_2, "=", divide(number_1, number_2))
+        
