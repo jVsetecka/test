fb8c6be3df38cddc6f073b751754b3e5a8f33363
Jakub Vsetecka <jvsetecka@segment1.org>
2023-12-04 08:45:05 +0100

    feat: functions added

--- python.py
+++ python.py
@@ -1,1 +1,15 @@
-print("Hello world")
+def add(num1, num2):
+    return num1 + num2
+
+def subtract(num1, num2):
+    return num1 - num2
+
+def multiply(num1, num2):
+    return num1 * num2
+
+def divide(num1, num2):
+    if num2 != 0:
+        return num1 / num2
+    else:
+        return "Error: Division by zero is not allowed"
+
